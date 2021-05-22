ef9c208882ae17deb0c074bfc8ad6e1b3890e894
skycfwu <wcf0118@gmail.com>
2021-05-22 10:23:33 +0800

    工厂方法初始化

---
+++ design_pattern/factory_method.cpp
@@ -0,0 +1,24 @@
+#include "factory_method.h"
+
+
+CCfgParse::CCfgParse()
+{
+
+}
+
+CCfgParse::~CCfgParse()
+{
+
+}
+
+CXmlCfgParse::CXmlCfgParse()
+{
+    
+}
+
+CXmlCfgParse::~CXmlCfgParse()
+{
+
+}
+
+
