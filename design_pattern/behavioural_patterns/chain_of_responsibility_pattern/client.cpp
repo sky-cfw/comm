df1546b3b302079ccdeeecec67967e10a59a3f3f
sky <wcf0118@gmail.com>
2021-09-12 01:41:29 +0800

    面向对象设计模式对象行为型之责任链模式构造handler链变种

--- design_pattern/behavioural_patterns/chain_of_responsibility_pattern/client.cpp
+++ design_pattern/behavioural_patterns/chain_of_responsibility_pattern/client.cpp
@@ -10,18 +10,40 @@ do {\
     }\
 }while(0);
 
+void CClient::AddHandler( CHandler *&pCHandler )
+{
+    m_vecHandler.push_back( pCHandler );
+}
+
+void CClient::DoHandle( stRequest &tRequest )
+{
+    for ( auto &pCHandler : m_vecHandler )
+        pCHandler->DoHandle( tRequest );
+}
+
 void CClient::DoSomething()
 {
     CHandler *pConcreteHandler1 = new CConcreteHandler1();
     CHandler *pConcreteHandler2 = new CConcreteHandler2();
     CHandler *pConcreteHandler3 = new CConcreteHandler3();
 
+    //构造handler链方式1
     //构造处理责任链，注意顺序 (这里处理顺序是1->2->3)
-    pConcreteHandler1->SetNext( pConcreteHandler2 );//依赖注入/组合/委托
-    pConcreteHandler2->SetNext( pConcreteHandler3 );
+    // pConcreteHandler1->SetNext( pConcreteHandler2 );//依赖注入/组合/委托
+    // pConcreteHandler2->SetNext( pConcreteHandler3 );
+
+    // stRequest tRequest;
+    // pConcreteHandler1->DoHandle( tRequest );
+    //end 构造handler链方式1
+
+    //构造handler链方式2
+    AddHandler( pConcreteHandler1 );
+    AddHandler( pConcreteHandler2 );
+    AddHandler( pConcreteHandler3 );
 
     stRequest tRequest;
-    pConcreteHandler1->DoHandle( tRequest );
+    DoHandle(tRequest);
+    //end 构造handler链方式2
 
     RELEASE_RESOURCE(pConcreteHandler1)
     RELEASE_RESOURCE(pConcreteHandler2)
--- design_pattern/behavioural_patterns/chain_of_responsibility_pattern/client.h
+++ design_pattern/behavioural_patterns/chain_of_responsibility_pattern/client.h
@@ -1,10 +1,17 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#include <vector>
+#include "handler.h"
 class CClient
 {
 public:
     void DoSomething();
+
+    //责任链模式变种，单独使用一个数组来存储handler链
+    void AddHandler( CHandler *&pCHandler );
+    void DoHandle( stRequest &tRequest );
+    std::vector<CHandler *> m_vecHandler;
 };
 
 #endif
