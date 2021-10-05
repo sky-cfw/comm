99c7715e39eed3057f10124a39b251e37aaa43a1
sky <wcf0118@gmail.com>
2021-10-05 19:45:42 +0800

    新增文件映射IO测试

---
+++ linux/file_io/mmap_file_io.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    if ( argc < 2 )
+    {
+        printf("Usage: ./mmap_file_io <1> file_name \n");
+        return 0;
+    }
+
+    //file content: this is mmap file io test.
+    int fd = open( argv[2], O_RDWR );
+    if ( -1 == fd )
+    {
+        perror( "open" );
+        return -1;
+    }
+
+    struct stat statbuf;
+    if ( -1 == fstat( fd, &statbuf ) )
+    {
+        perror( "fstat" );
+        return -1;
+    }
+
+    char *addr = (char *)mmap( NULL, statbuf.st_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0 );
+    close(fd);//关闭文件，并不影响addr
+    if ( addr == MAP_FAILED )
+    {
+        perror( "mmap" );
+        return -1;
+    }
+    strncpy( addr, "that", 4 );
+
+    munmap(addr, statbuf.st_size);
+
+    return 0;
+}
