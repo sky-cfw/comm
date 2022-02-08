5f78b3c302bf9bce38dec5ecb9352e748d31fc6d
sky <wcf0118@gmail.com>
2022-02-08 14:09:31 +0800

    单服务器百万长连接

--- linux/network/tcp_client_c10k.go
+++ linux/network/tcp_client_c10k.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-07 18:53:36
- * @LastEditTime: 2022-02-07 23:21:33
+ * @LastEditTime: 2022-02-08 14:08:04
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /comm/linux/network/tcp_client_c10k.go
@@ -11,9 +11,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"time"
 )
+
+func init() {
+	//设置输出选项
+	log.SetFlags(log.Lshortfile | log.LstdFlags | log.Lmicroseconds)
+
+	//设置输出位置
+	logFile, err := os.OpenFile("./tcp_client_c10k.log", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		log.Panic("打开日志文件异常")
+		return
+	}
+
+	log.SetOutput(logFile)
+}
 
 func main() {
 	argc := len(os.Args)
@@ -22,19 +38,25 @@ func main() {
 		return
 	}
 
-	fmt.Println("start to conn server ...")
 	addr := os.Args[1] + ":" + os.Args[2]
 	cnt := 0
 
+	fmt.Println("start to conn server ...")
+	log.Println("start to conn server ...")
+	start := time.Now()
 	for {
 		_, err := net.Dial("tcp", addr)
 		if err != nil {
-			fmt.Println("Dial failed", err.Error())
-			fmt.Println("conn total num", cnt)
+			log.Println("Dial failed", err.Error())
+			log.Println("conn total num", cnt)
 			return
 		}
 
 		cnt++
-		fmt.Println("conn num", cnt)
+		if 0 == cnt%10 {
+			cost := time.Since(start)
+			log.Printf("conn num [%d], time[%s]", cnt, cost)
+			start = time.Now()
+		}
 	}
 }
--- linux/network/tcp_server.go
+++ linux/network/tcp_server.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-15 23:18:16
- * @LastEditTime: 2022-02-07 23:19:00
+ * @LastEditTime: 2022-02-08 14:07:08
  * @LastEditors: Please set LastEditors
  * @Description: tcp监听服务器
  * @FilePath: /comm/linux/network/tcp_server.go
@@ -10,9 +10,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"runtime"
+	"time"
 	// log "github.com/sirupsen/logrus"
 )
 
@@ -27,6 +29,19 @@ import (
 // 	// Only log the warning severity or above.
 // 	log.SetLevel(log.WarnLevel)
 // }
+func init() {
+	//设置输出选项
+	log.SetFlags(log.Lshortfile | log.LstdFlags | log.Lmicroseconds)
+
+	//设置输出位置
+	logFile, err := os.OpenFile("./tcp_client_c10k.log", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		log.Panic("打开日志文件异常")
+		return
+	}
+
+	log.SetOutput(logFile)
+}
 
 func main() {
 	fmt.Println(runtime.Version())
@@ -37,25 +52,31 @@ func main() {
 	}
 
 	//
-	fmt.Println("starting the server")
 	addr := ":" + os.Args[1]
+	// fmt.Println("starting the server")
+	log.Println("starting the server")
 	ls, err := net.Listen("tcp", addr)
 	if err != nil {
-		fmt.Println("err listen", err.Error())
+		//fmt.Println("err listen", err.Error())
+		log.Println("err listen", err.Error())
 		return
 	}
 
 	cnt := 0 //短变量声明
-
+	start := time.Now()
 	for {
 		_, err := ls.Accept()
 		if err != nil {
-			fmt.Println("err accept", err.Error())
-			fmt.Println("recv conn total num", cnt)
+			log.Println("err accept", err.Error())
+			log.Println("recv conn total num", cnt)
 			return
 		}
 
 		cnt++
-		fmt.Println("conn num", cnt)
+		if 0 == cnt%10 {
+			cost := time.Since(start)
+			log.Printf("conn num [%d], cost [%s]", cnt, cost)
+			start = time.Now()
+		}
 	}
 }
