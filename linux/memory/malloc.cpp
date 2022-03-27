9c65098962f4a08a2ecca957e6cb718ba488a6bf
sky <wcf0118@gmail.com>
2022-03-27 13:46:18 +0800

    新增malloc内存分配原理测试demo

---
+++ linux/memory/malloc.cpp
@@ -0,0 +1,36 @@
+/*
+ * @Author: sky
+ * @Date: 2022-03-19 22:04:31
+ * @LastEditTime: 2022-03-27 13:42:24
+ * @LastEditors: Please set LastEditors
+ * @Description: malloc 内存分配原理
+ * @FilePath: /comm/linux/memory/malloc.cpp
+ */
+
+/*
+Q1：malloc(1) 会分配多大的内存？
+Q2：free释放内存，会归还给操作系统吗？
+*/
+
+#include <stdio.h>
+#include <malloc.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    //申请1字节的内存空间
+    void *addr = malloc(1);
+    //void *addr = malloc( 128*1024 );
+    printf( "此1字节的内存起始地址为: %p\n", addr );
+    printf( "使用 cat /proc/%d/maps查看内存分配\n", getpid() );
+
+    //将程序阻塞，当输入任意字符时才往下执行
+    getchar();
+
+    //释放内存
+    free(addr);
+    printf( "释放了1字节的堆内存，但并不会归还给操作系统\n" );
+    getchar();
+
+    return 0;
+}
